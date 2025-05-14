000a869e0f005405dd094d25fedbd055b8bf9b80
AMattiza <andreas@choose-hub.com>
2025-05-14 20:55:35 +0200

    Update InputMask.jsx

--- src/components/InputMask.jsx
+++ src/components/InputMask.jsx
@@ -54,7 +54,7 @@ export default function InputMask({ data, onChange, sections }) {
             {group.section}
           </legend>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-60">
             {group.items.map(item => (
               <div key={item.name} className="flex flex-col">
                 <label
